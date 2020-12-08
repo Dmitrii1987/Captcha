35d7333cb7724229b61b4113578d99be70d61822
Dmitrii1987 <62569977+Dmitrii1987@users.noreply.github.com>
2020-12-08 18:07:12 +0300

    Update captcha.js
    some lines added

--- captcha.js
+++ captcha.js
@@ -1,3 +1,7 @@
 let captcha;
 let alphabets = "AaBbCcDdFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
 let status = document.getElementById('status');
+generateCaptcha = () => {
+    let first = Math.floor((Math.random() *10));
+    let second = alphabets[Math.floor((Math.random() * alphabets.length))];
+}
